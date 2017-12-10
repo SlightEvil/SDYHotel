2ba50c32a94e435cb97bca8e3ab00df60aac5567
admin <admin@admindemacbook-air.local>
2017-12-10 09:48:17 +0800

    Initial Commit

---
+++ SDYHotel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SDYHotel
+//
+//  Created by admin on 2017/12/10.
+//  Copyright © 2017年 SanDaoYi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
